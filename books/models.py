82737912e7d175ca06f92226bcfb0563515452d1
Victormuthomi <victormuhomi100@gmail.com>
2024-09-20 00:56:47 +0300

    Initial commit.

---
+++ books/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    """The books fields"""
+    title = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    author = models.CharField(max_length=100)
+    isbn = models.CharField(max_length=13)
